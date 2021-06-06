c4f2bda5596dea76073804412d6cd52e346a3a74
jjdyson <>
2021-06-06 23:19:50 +0000

    Error checking and completely changed code due to function not calling within GitHub.

--- candidate-testing.js
+++ candidate-testing.js
@@ -6,14 +6,12 @@ const input = require('readline-sync');
 let candidateName = "";
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space? ";
+let question = "";
 let correctAnswer = "Sally Ride"; 
 let candidateAnswer = "";
 let questions =  [ "1) Who was the first American woman in space? ", "2) True or false: 5 kilometer == 5000 meters? ", "3) (5 + 3)/2 * 10 =? ", "4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "5) What is the minimum crew size for the ISS? " ];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
-
-
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -27,15 +25,12 @@ function askQuestion() {
   for (let i = 0; i < questions.length; i++) {
     //console.log(i);  
     candidateAnswers[i] = input.question(questions[i]);
-    // candidateAnswers.push(candidateAnswers);
     console.log(`Your Answer: ${candidateAnswers[i]}`);
     console.log(`Correct Answer: ${correctAnswers[i]}\n`);
   } 
  }
 
-
-
-  
+ 
 function gradeQuiz(candidateAnswers) {
 
   //TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
@@ -63,14 +58,11 @@ let status = "PASSED";
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-     console.log("candidateName: " + candidateName);
+     //console.log("candidateName: " + candidateName);
 
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
-
-
-
 
 //Don't write any code below this line //
 //And don't change these or your program will not run as expected //
