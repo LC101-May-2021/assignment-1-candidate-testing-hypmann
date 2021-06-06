38c56bd032f320343cfc875cf84ed0e620174984
jjdyson <>
2021-06-06 16:56:26 +0000

    Error checking code.

--- candidate-testing.js
+++ candidate-testing.js
@@ -9,7 +9,7 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride"; 
 let candidateAnswer = "";
-let questions =  [ "1) Who was the first American woman in space? ", "\n2) True or false: 5 kilometer == 5000 meters? ", "\n3) (5 + 3)/2 * 10 = ? ", "\n4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "\n5) What is the minimum crew size for the ISS? " ];
+let questions =  [ "1) Who was the first American woman in space? ", "\n2) True or false: 5 kilometer == 5000 meters? ", "\n3) (5 + 3)/2 * 10 =? ", "\n4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "\n5) What is the minimum crew size for the ISS? " ];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 let totalCorrectAnswers = 0;
@@ -53,19 +53,20 @@ function gradeQuiz(candidateAnswers) {
       console.log(`>>> Overall Grade: ${grade}% (${totalCorrectAnswers} of ${questions.length} responses correct) <<<`);
       console.log();
       console.log(">>> Status: Passed <<<");
-
+      return grade;
+      
      } else {
      console.log(`>>> Overall Grade: ${grade}% (${totalCorrectAnswers} of ${questions.length} responses correct) <<<`);
      console.log(">>> Status: Failed <<<");
-     
+     return grade;
     }
-    
-    return grade;
-}
-   
+    //return grade;
+
+  }
    myCompleteGrade();
 
 }
+
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
